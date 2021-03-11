9c1280b4d9c1ddce5163163af6de0d65a72267bd
Henrique Teofilo <henrique@teofilo.me>
2021-03-10 21:05:50 -0300

    Adding relay flag and some code comments on VersionMessage

--- src/main/java/bitcoinlistener/messages/VersionMessage.java
+++ src/main/java/bitcoinlistener/messages/VersionMessage.java
@@ -15,21 +15,27 @@ import bitcoinlistener.util.ByteUtil;
  */
 public class VersionMessage implements ProtocolMessage {
 
-	private int version;         // int32_t (4)
-	private long services;       // uint64_t (8)
-	private long time;           // int64_t  (8)
-	private NetAddress addrRecv; // net_addr (26)
-	private NetAddress addrFrom; // net_addr (26)
-	private long nonce;          // uint64_t (8) 
-	private String subVer;       // VarString (?)
-	private int startHeight;     // int32_t (4)
+	private int version;           // int32_t   (4)
+	private long services;         // uint64_t  (8)
+	private long time;             // int64_t   (8)
+	private NetAddress addrRecv;   // net_addr  (26)
+	private NetAddress addrFrom;   // net_addr  (26)
+	private long nonce;            // uint64_t  (8) 
+	private String subVer;         // VarString (?)
+	private int startHeight;       // int32_t   (4)
+	private boolean relay;         // bool      (1)
+	
+	
+	// =============================================================================================
+	// CONSTRUCTORS                                                                                
+	// =============================================================================================
 	
 	public VersionMessage() {
 		
 	}
 
 	public VersionMessage(int version, long services, long time, NetAddress addrRecv,
-			NetAddress addrFrom, long nonce, String subVer, int startHeight) {
+			NetAddress addrFrom, long nonce, String subVer, int startHeight, boolean relay) {
 		super();
 		this.version = version;
 		this.services = services;
@@ -39,19 +45,27 @@ public class VersionMessage implements ProtocolMessage {
 		this.nonce = nonce;
 		this.subVer = subVer;
 		this.startHeight = startHeight;
+		this.relay = relay;
 	}
 
 	
 	public VersionMessage(int version, String subVer) {
 		this.version = version;
-		this.services = 1;
+		this.services = 0;
 		this.time = System.currentTimeMillis() / 1000L; // unix timestamp
 		this.addrRecv = new NetAddress("127.0.0.1", 8333);
 		this.addrFrom = new NetAddress();
 		this.nonce = ByteUtil.getRandomNonce();
 		this.subVer = subVer;
 		this.startHeight = -1;
+		this.relay = true;
 	}
+
+	
+	// =============================================================================================
+	/// OPERATIONS                                                                                
+	// =============================================================================================
+	
 
 	public byte[] getBytes() {
 		BitcoinBuffer buf = new BitcoinBuffer(10000);
@@ -66,13 +80,71 @@ public class VersionMessage implements ProtocolMessage {
 		VarString v = new VarString(this.subVer);
 		v.writeToBuffer(buf);
 		
-		buf.putInt32(this.startHeight);                  // 4
+		buf.putInt32(this.startHeight);                 // 4
 
+		if (version > 70001) {
+			buf.putBoolean(this.relay);                 // 1
+		}
+		
 		byte[] ret = buf.toArrayExactSize();
 
 		return ret;
 	}
+	
+	@Override
+	public void loadFromBuffer(BitcoinBuffer buf) {
+		ByteOrder o = buf.getEndianness();
+		try {
+			buf.setEndianness(ByteOrder.LITTLE_ENDIAN);
+			
+			int version = buf.getInt32();
+			long services = buf.getUint64().longValue();
+			long time = buf.getInt64();
 
+			NetAddress addrRecv = new NetAddress();
+			addrRecv.loadFromBuffer(buf);
+
+			
+			NetAddress addrFrom = new NetAddress();
+			addrFrom.loadFromBuffer(buf);
+
+			long nonce = buf.getUint64().longValue();
+
+			
+			VarString vStr = new VarString();
+			vStr.loadFromBuffer(buf);
+			String str = vStr.getString();
+
+
+			int height = -1;
+			if (version >= 209) {
+				height = buf.getInt32();
+			}
+
+			// Transaction relay flag
+			boolean relay = true;
+			if (version > 70001) {
+				relay = buf.getBoolean();
+			}
+
+			this.version = version;
+			this.services = services;
+			this.time = time;
+			this.addrRecv = addrRecv;
+			this.addrFrom = addrFrom;
+			this.nonce = nonce;
+			this.subVer = str;
+			this.startHeight = height;
+			this.relay = relay;
+
+		} finally {
+			buf.setEndianness(o);
+		}
+	}
+	
+	// =============================================================================================
+	// ACCESSORS (GETTERS AND SETTERS)                                                              
+	// =============================================================================================
 
 	public int getVersion() {
 		return version;
@@ -144,59 +216,15 @@ public class VersionMessage implements ProtocolMessage {
 	}
 
 	
-	@Override
-	public void loadFromBuffer(BitcoinBuffer buf) {
-		ByteOrder o = buf.getEndianness();
-		try {
-			buf.setEndianness(ByteOrder.LITTLE_ENDIAN);
-			
-			int version = buf.getInt32();
-			long services = buf.getUint64().longValue();
-			long time = buf.getInt64();
-
-			NetAddress addrRecv = new NetAddress();
-			addrRecv.loadFromBuffer(buf);
-
-			
-			NetAddress addrFrom = new NetAddress();
-			addrFrom.loadFromBuffer(buf);
-
-			long nonce = buf.getUint64().longValue();
-
-			
-			VarString vStr = new VarString();
-			vStr.loadFromBuffer(buf);
-			String str = vStr.getString();
-
-
-			int height = -1;
-			if (version >= 209) {
-				height = buf.getInt32();
-			}
-
-			// Transaction relay flag
-			if (version > 70001) {
-				byte b = buf.getByte();
-			}
-
-			this.version = version;
-			this.services = services;
-			this.time = time;
-			this.addrRecv = addrRecv;
-			this.addrFrom = addrFrom;
-			this.nonce = nonce;
-			this.subVer = str;
-			this.startHeight = height;
-
-		} finally {
-			buf.setEndianness(o);
-		}
-	}
-
+	// =============================================================================================
+	/// OBJECT OPERATIONS                                                                           
+	// =============================================================================================
+	
 	@Override
 	public String toString() {
 		return getClass().getSimpleName() + "[version=" + version + ", services=" + services + 
-				", time=" + time + ", addrRecv=" + addrRecv + ", addrFrom=" + addrFrom +
-				", nonce=" + nonce + ", subVer=" + subVer + ", startHeight=" + startHeight + "]";
+				", time=" + time + ", addrRecv=" + addrRecv + ", addrFrom=" + addrFrom + 
+				", nonce=" + nonce + ", subVer=" + subVer + ", startHeight=" + startHeight + 
+				", relay=" + relay + "]";
 	}
 }
