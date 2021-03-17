fce3a76e6270b855232b38481e7dfae7e790b7c6
Henrique Teofilo <henrique@teofilo.me>
2021-03-16 21:48:10 -0300

    add regtest network

--- src/main/java/bitcoinlistener/NetworkParameters.java
+++ src/main/java/bitcoinlistener/NetworkParameters.java
@@ -11,8 +11,8 @@ package bitcoinlistener;
 public enum NetworkParameters {
 	
 	MainNet("MAINNET", new byte[] { (byte) 0xF9, (byte) 0XBE, (byte) 0XB4, (byte) 0xD9 }),
-	TestNet3("TESTNET3", new byte[] { (byte) 0x0B, (byte) 0x11, (byte) 0x09, (byte) 0x07 });
-	
+	TestNet3("TESTNET3", new byte[] { (byte) 0x0B, (byte) 0x11, (byte) 0x09, (byte) 0x07 }),
+	RegTest("REGTEST", new byte[] { (byte) 0xFA, (byte) 0xBF, (byte) 0xB5, (byte) 0xDA });
 	
 	private String name;
 	private byte[] magicValue;
